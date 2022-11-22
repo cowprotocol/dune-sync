ed34422c92b32e4618a7df3667b5dd9aa487a573
Benjamin Smith <bh2smith@users.noreply.github.com>
2022-11-22 14:37:48 +0100

    [Fetch] Orderbook Rewards (#3)
    We migrate a query we have seen before in solver-rewards (namely the one querying for orderbook rewards on a block range). This block range will be specified as:
    
    block_from = the latest synced block (coming in a follow up PR)
    block_to = the latest synced block from the two orderbook databases
    
    block_to is implemented here by querying the two databases for the smallest block with tx_from = Null taking the minimum of these two and subtracting 65 (for REORG protection)

---
+++ src/fetch/orderbook.py
@@ -0,0 +1,88 @@
+"""Basic client for connecting to postgres database with login credentials"""
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+from enum import Enum
+
+import pandas as pd
+from dotenv import load_dotenv
+from pandas import DataFrame
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.exc import ProgrammingError
+
+from src.models.block_range import BlockRange
+from src.utils import open_query
+
+REORG_THRESHOLD = 65
+
+
+class OrderbookEnv(Enum):
+    """
+    Enum for distinguishing between CoW Protocol's staging and production environment
+    """
+
+    BARN = "BARN"
+    PROD = "PROD"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+@dataclass
+class OrderbookFetcher:
+    """
+    A pair of Dataframes primarily intended to store query results
+    from production and staging orderbook databases
+    """
+
+    @staticmethod
+    def _pg_engine(db_env: OrderbookEnv) -> Engine:
+        """Returns a connection to postgres database"""
+        load_dotenv()
+        db_url = os.environ[f"{db_env}_DB_URL"]
+        db_string = f"postgresql+psycopg2://{db_url}"
+        return create_engine(db_string)
+
+    @staticmethod
+    def _exec_query(query: str, engine: Engine) -> DataFrame:
+        try:
+            return pd.read_sql(
+                sql=query.replace("{{reward_table}}", "order_execution"), con=engine
+            )
+        except ProgrammingError:
+            return pd.read_sql(
+                sql=query.replace("{{reward_table}}", "order_rewards"), con=engine
+            )
+
+    @classmethod
+    def _query_both_dbs(cls, query: str) -> tuple[DataFrame, DataFrame]:
+        barn = cls._exec_query(query, engine=cls._pg_engine(OrderbookEnv.BARN))
+        prod = cls._exec_query(query, engine=cls._pg_engine(OrderbookEnv.PROD))
+        return barn, prod
+
+    @classmethod
+    def get_latest_block(cls) -> int:
+        """
+        Fetches the latest mutually synced block from orderbook databases (with REORG protection)
+        """
+        barn, prod = cls._query_both_dbs(open_query("orderbook/latest_block.sql"))
+        assert len(barn) == 1 == len(prod), "Expecting single record"
+        return min(int(barn["latest"][0]), int(prod["latest"][0])) - REORG_THRESHOLD
+
+    @classmethod
+    def get_orderbook_rewards(cls, block_range: BlockRange) -> DataFrame:
+        """
+        Fetches and validates Orderbook Reward DataFrame as concatenation from Prod and Staging DB
+        """
+        cow_reward_query = (
+            open_query("orderbook/order_rewards.sql")
+            .replace("{{start_block}}", str(block_range.block_from))
+            .replace("{{end_block}}", str(block_range.block_to))
+        )
+        barn, prod = cls._query_both_dbs(cow_reward_query)
+
+        # Solvers do not appear in both environments!
+        assert set(prod.solver).isdisjoint(set(barn.solver)), "solver overlap!"
+        return pd.concat([prod, barn])
