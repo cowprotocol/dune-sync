f1943e401c23211aa508c06df6e7838c178bd546
Ben Smith <bh2smith@gmail.com>
2022-11-29 19:28:45 +0100

    full independent tests of upload, delete and download

--- tests/integration/test_aws.py
+++ tests/integration/test_aws.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from os.path import exists
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -18,16 +19,28 @@ class TestAWSConnection(unittest.TestCase):
         )
         self.empty_file = "file.json"
         self.key = f"test/{self.empty_file}"
-        Path(self.empty_file).touch()
 
     def tearDown(self) -> None:
-        os.remove(Path(self.empty_file))
+        try:
+            os.remove(Path(self.empty_file))
+        except FileNotFoundError:
+            pass
 
     def test_assumed_role(self):
         s3_resource = self.aws_client._assume_role()
         self.assertIsNotNone(s3_resource.buckets)
 
+    def create_upload_remove(self):
+        Path(self.empty_file).touch()
+        self.aws_client.upload_file(
+            filename=self.empty_file,
+            bucket=self.bucket,
+            object_key=f"test/{self.empty_file}",
+        )
+        os.remove(Path(self.empty_file))
+
     def test_upload_file(self):
+        Path(self.empty_file).touch()
         success = self.aws_client.upload_file(
             filename=self.empty_file,
             bucket=self.bucket,
@@ -36,12 +49,23 @@ class TestAWSConnection(unittest.TestCase):
         self.assertTrue(success)
 
     def test_download_file(self):
+        # Upload file and remove it from our filesystem
+        self.create_upload_remove()
+
+        with self.assertRaises(FileNotFoundError):
+            open(self.empty_file)
+
         success = self.aws_client.download_file(
-            filename=self.empty_file, bucket=self.bucket, object_key=self.key
+            filename=self.empty_file,
+            bucket=self.bucket,
+            object_key=self.key,
         )
         self.assertTrue(success)
+        # File was downloaded!
+        self.assertTrue(exists(self.empty_file))
 
     def test_delete_file(self):
+        self.create_upload_remove()
         success = self.aws_client.delete_file(bucket=self.bucket, object_key=self.key)
         self.assertTrue(success)
 
