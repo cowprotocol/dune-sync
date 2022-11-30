1913c91ed795c1331b50f8434dde4c1ee6276f8e
Ben Smith <bh2smith@gmail.com>
2022-11-30 11:03:48 +0100

    sync orderbook data

--- src/main.py
+++ src/main.py
@@ -1,37 +1,80 @@
 """Main Entry point for app_hash sync"""
+import argparse
 import asyncio
 import logging.config
 import os
+from enum import Enum
 from pathlib import Path
 
 from dotenv import load_dotenv
 
 from src.environment import PROJECT_ROOT
+from src.fetch.orderbook import OrderbookFetcher
 from src.sync import sync_app_data
 from src.fetch.dune import DuneFetcher
-from src.sync.config import AppDataSyncConfig, AWSData
+from src.sync.config import SyncConfig, AWSData
+from src.sync.order_rewards import sync_order_rewards
 
 log = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
 log.setLevel(logging.DEBUG)
 
 
-GIVE_UP_THRESHOLD = 10
+class SyncTable(Enum):
+    """Enum for Deployment Supported Table Sync"""
+
+    APP_DATA = "app_data"
+    ORDER_REWARDS = "order_rewards"
+
+
+def script_args() -> argparse.Namespace:
+    """Parse runtime arguments"""
+    parser = argparse.ArgumentParser("Dune Community Sources Sync")
+    parser.add_argument(
+        "--sync-table",
+        type=SyncTable,
+        choices=list(SyncTable),
+    )
+    parser.add_argument(
+        "--dry-run",
+        type=bool,
+        help="Flag indicating whether script should not post files to AWS or not",
+        default=False,
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
     load_dotenv()
-    asyncio.run(
-        sync_app_data(
-            dune=DuneFetcher(os.environ["DUNE_API_KEY"]),
-            config=AppDataSyncConfig(
-                aws=AWSData(
-                    internal_role=os.environ["AWS_INTERNAL_ROLE"],
-                    external_role=os.environ["AWS_EXTERNAL_ROLE"],
-                    external_id=os.environ["AWS_EXTERNAL_ID"],
-                    bucket=os.environ["AWS_BUCKET"],
+    aws = AWSData(
+        internal_role=os.environ["AWS_INTERNAL_ROLE"],
+        external_role=os.environ["AWS_EXTERNAL_ROLE"],
+        external_id=os.environ["AWS_EXTERNAL_ID"],
+        bucket=os.environ["AWS_BUCKET"],
+    )
+    volume_path = PROJECT_ROOT / Path(os.environ.get("VOLUME_PATH", "data"))
+
+    args = script_args()
+    sync_table = args.sync_table
+
+    if sync_table == SyncTable.APP_DATA:
+        asyncio.run(
+            sync_app_data(
+                dune=DuneFetcher(os.environ["DUNE_API_KEY"]),
+                config=SyncConfig(
+                    aws,
+                    volume_path,
+                    table_name=str(sync_table),
                 ),
-                volume_path=PROJECT_ROOT / Path(os.environ.get("VOLUME_PATH", "data")),
+                missing_file_name="missing_app_hashes.json",
+            )
+        )
+    elif sync_table == SyncTable.ORDER_REWARDS:
+        sync_order_rewards(
+            fetcher=OrderbookFetcher(),
+            config=SyncConfig(
+                aws,
+                volume_path,
+                table_name=str(sync_table),
             ),
         )
-    )
--- src/sync/app_data.py
+++ src/sync/app_data.py
@@ -1,10 +1,7 @@
 """Main Entry point for app_hash sync"""
 import json
 import logging.config
-import os.path
-import sys
 
-from boto3.exceptions import S3UploadFailedError
 from dune_client.file.interface import FileIO
 from dune_client.types import DuneRecord
 
@@ -12,7 +9,8 @@ from src.fetch.dune import DuneFetcher
 from src.fetch.ipfs import Cid
 from src.models.block_range import BlockRange
 from src.post.aws import AWSClient
-from src.sync.config import AppDataSyncConfig
+from src.sync.common import last_sync_block, aws_login_and_upload
+from src.sync.config import SyncConfig
 
 log = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
@@ -29,14 +27,16 @@ class RecordHandler:  # pylint:disable=too-many-instance-attributes
     it attempts to fetch content for them and filters them into "found" and "not found" as necessary
     """
 
-    def __init__(
+    def __init__(  # pylint: disable=too-many-arguments
         self,
         file_manager: FileIO,
         new_rows: list[DuneRecord],
         block_range: BlockRange,
-        config: AppDataSyncConfig,
+        config: SyncConfig,
+        missing_file_name: str,
     ):
         self.file_manager = file_manager
+
         self.config = config
         self.block_range = block_range
         self.aws_client = AWSClient(
@@ -44,14 +44,15 @@ class RecordHandler:  # pylint:disable=too-many-instance-attributes
             external_role=config.aws.external_role,
             external_id=config.aws.external_id,
         )
+
         self._found: list[dict[str, str]] = []
         self._not_found: list[dict[str, str]] = []
 
+        self.content_filename = f"cow_{self.block_range.block_to}.json"
         self.new_rows = new_rows
+        self.missing_file_name = missing_file_name
         try:
-            self.missing_values = self.file_manager.load_ndjson(
-                config.missing_files_name
-            )
+            self.missing_values = self.file_manager.load_ndjson(missing_file_name)
         except FileNotFoundError:
             self.missing_values = []
 
@@ -115,9 +116,9 @@ class RecordHandler:  # pylint:disable=too-many-instance-attributes
                 row.update({"attempts": str(attempts)})
                 self._not_found.append(row)
 
-    def _write_found_content(self, content_filename: str) -> None:
+    def _write_found_content(self) -> None:
         assert len(self.new_rows) == 0, "Must call _handle_new_records first!"
-        self.file_manager.write_ndjson(data=self._found, name=content_filename)
+        self.file_manager.write_ndjson(data=self._found, name=self.content_filename)
 
     def _write_sync_data(self) -> None:
         # Only write these if upload was successful.
@@ -128,7 +129,7 @@ class RecordHandler:  # pylint:disable=too-many-instance-attributes
         # When not_found is empty, we want to overwrite the file (hence skip_empty=False)
         # This happens when number of attempts exceeds GIVE_UP_THRESHOLD
         self.file_manager.write_ndjson(
-            self._not_found, self.config.missing_files_name, skip_empty=False
+            self._not_found, self.missing_file_name, skip_empty=False
         )
 
     def fetch_content_and_filter(
@@ -147,28 +148,22 @@ class RecordHandler:  # pylint:disable=too-many-instance-attributes
 
     def write_and_upload_content(self) -> None:
         """
-        - Writes self.found to persistent volume,
+        - Writes self._found to persistent volume,
         - attempts to upload to AWS and
         - records last sync block on volume.
         """
-        content_filename = f"cow_{self.block_range.block_to}.json"
-        self._write_found_content(content_filename)
+        self._write_found_content()
 
         if len(self._found) > 0:
-            config = self.config
-            try:
-                self.aws_client.upload_file(
-                    filename=os.path.join(self.file_manager.path, content_filename),
-                    bucket=config.aws.bucket,
-                    object_key=f"{config.table_name}/{content_filename}",
-                )
-                log.info(
-                    f"App Data Sync for block range {self.block_range} complete: "
-                    f"synced {len(self._found)} records with {len(self._not_found)} missing"
-                )
-            except S3UploadFailedError as err:
-                logging.error(err)
-                sys.exit(1)
+            aws_login_and_upload(
+                config=self.config,
+                path=self.file_manager.path,
+                filename=self.content_filename,
+            )
+            log.info(
+                f"App Data Sync for block range {self.block_range} complete: "
+                f"synced {len(self._found)} records with {len(self._not_found)} missing"
+            )
         else:
             log.info(
                 f"No new App Data for block range {self.block_range}: no sync necessary"
@@ -177,48 +172,21 @@ class RecordHandler:  # pylint:disable=too-many-instance-attributes
         self._write_sync_data()
 
 
-async def get_block_range(
-    file_manager: FileIO, dune: DuneFetcher, last_block_file: str, column: str
-) -> BlockRange:
-    """
-    Constructs a block range object
-    block_from is fetched from the last sync block (via file_manager)
-    block_to is fetched from Dune via query results.
-    """
-
-    block_from = 12153262  # Genesis block
-    try:
-        block_from = int(file_manager.load_singleton(last_block_file, "csv")[column])
-    except FileNotFoundError:
-        log.warning(
-            f"last sync file {last_block_file} not found, using genesis block {block_from}"
-        )
-    except KeyError as err:
-        message = (
-            f"last sync file {last_block_file} does not contain column header {column}, "
-            f"exiting to avoid duplication"
-        )
-        log.error(message)
-        raise RuntimeError(message) from err
-
-    return BlockRange(
-        # TODO - could be replaced by Dune Query on the app_data table (once available).
-        #  https://github.com/cowprotocol/dune-bridge/issues/42
-        block_from,
-        block_to=await dune.latest_app_hash_block(),
-    )
-
-
-async def sync_app_data(dune: DuneFetcher, config: AppDataSyncConfig) -> None:
+async def sync_app_data(
+    dune: DuneFetcher, config: SyncConfig, missing_file_name: str
+) -> None:
     """App Data Sync Logic"""
     # TODO - assert legit configuration before proceeding!
     table_name = config.table_name
     file_manager = FileIO(config.volume_path / table_name)
-    block_range = await get_block_range(
-        file_manager,
-        dune,
-        last_block_file=config.sync_file,
-        column=config.sync_column,
+    block_range = BlockRange(
+        block_from=last_sync_block(
+            file_manager,
+            last_block_file=config.sync_file,
+            column=config.sync_column,
+            genesis_block=12153262,  # First App Hash Block
+        ),
+        block_to=await dune.latest_app_hash_block(),
     )
 
     data_handler = RecordHandler(
@@ -226,6 +194,7 @@ async def sync_app_data(dune: DuneFetcher, config: AppDataSyncConfig) -> None:
         new_rows=await dune.get_app_hashes(block_range),
         block_range=block_range,
         config=config,
+        missing_file_name=missing_file_name,
     )
     data_handler.fetch_content_and_filter(MAX_RETRIES)
     data_handler.write_and_upload_content()
