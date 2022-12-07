d9a66e453b40b9c30ea74003177c248e982d005b
Ben Smith <bh2smith@gmail.com>
2022-12-07 09:00:04 +0100

    fix the test

--- tests/e2e/test_sync_app_data.py
+++ tests/e2e/test_sync_app_data.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 from pathlib import Path
 from unittest import IsolatedAsyncioTestCase
@@ -19,27 +20,59 @@ class TestSyncAppData(IsolatedAsyncioTestCase):
         self.config = AppDataSyncConfig(
             volume_path=Path(os.environ["VOLUME_PATH"]),
             missing_files_name="missing_app_hashes.json",
-            max_retries=3,
-            give_up_threshold=10,
+            max_retries=2,
+            give_up_threshold=3,
         )
+        self.file_manager = FileIO(self.config.volume_path / str(SYNC_TABLE))
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.config.volume_path)
 
     async def test_fetch_content_and_filter(self):
-        block_range = BlockRange(
-            block_from=15082187,
-            block_to=16082187,
+        retries = self.config.max_retries
+        give_up = self.config.give_up_threshold
+        missing_files = self.config.missing_files_name
+        # block numbers
+        a, b, c = 15582187, 16082187, 16100000
+        self.assertTrue(a < b < c)
+
+        block_range_1 = BlockRange(
+            block_from=a,
+            block_to=b,
         )
         data_handler = AppDataHandler(
-            file_manager=FileIO(self.config.volume_path / str(SYNC_TABLE)),
-            new_rows=await self.dune.get_app_hashes(block_range),
-            block_range=block_range,
+            file_manager=self.file_manager,
+            new_rows=await self.dune.get_app_hashes(block_range_1),
+            block_range=block_range_1,
             config=self.config,
-            missing_file_name=self.config.missing_files_name,
+            missing_file_name=missing_files,
         )
-        max_retries = self.config.max_retries
-        give_up_threshold = self.config.give_up_threshold
 
-        await data_handler.fetch_content_and_filter(max_retries, give_up_threshold)
-        self.assertEqual(True, False)  # add assertion here
+        print(f"Beginning Content Fetching on {len(data_handler.new_rows)} records")
+        await data_handler.fetch_content_and_filter(retries, give_up)
+        data_handler.write_found_content()
+        self.assertEqual(0, len(data_handler.new_rows))
+
+        block_range_2 = BlockRange(
+            block_from=b,
+            block_to=c,
+        )
+        data_handler = AppDataHandler(
+            file_manager=self.file_manager,
+            new_rows=await self.dune.get_app_hashes(block_range_2),
+            block_range=block_range_2,
+            config=self.config,
+            missing_file_name=missing_files,
+        )
+        print(
+            f"Beginning Second Run Content Fetching on {len(data_handler.new_rows)} records"
+        )
+        await data_handler.fetch_content_and_filter(retries, give_up)
+        data_handler.write_found_content()
+
+        self.assertEqual(0, len(data_handler.new_rows))
+        # Two runs with retries = 2 and give_up = 3 implies no more missing records.
+        self.assertEqual(0, len(data_handler._not_found))
 
 
 if __name__ == "__main__":
