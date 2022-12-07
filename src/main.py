3667015abc79245a6d5a3c8d49094997e681fc8b
Ben Smith <bh2smith@gmail.com>
2022-12-07 21:01:28 +0100

    add hard reset feature to script entry point

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import argparse
 import asyncio
 import logging.config
 import os
+import shutil
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -26,6 +27,7 @@ class ScriptArgs:
     """Runtime arguments' parser/initializer"""
 
     dry_run: bool
+    hard_reset: bool
     sync_table: SyncTable
 
     def __init__(self) -> None:
@@ -42,9 +44,17 @@ class ScriptArgs:
             help="Flag indicating whether script should not post files to AWS or not",
             default=False,
         )
+        parser.add_argument(
+            "--hard-reset",
+            type=bool,
+            help="Flag indicating whether the existing sync table data should "
+                 "be deleted from the bucket and rebuilt from scratch",
+            default=False,
+        )
         arguments, _ = parser.parse_known_args()
         self.sync_table: SyncTable = arguments.sync_table
         self.dry_run: bool = arguments.dry_run
+        self.hard_reset: bool = arguments.hard_reset
 
 
 if __name__ == "__main__":
@@ -52,6 +62,12 @@ if __name__ == "__main__":
     volume_path = Path(os.environ["VOLUME_PATH"])
     args = ScriptArgs()
     aws = AWSClient.new_from_environment()
+    if args.hard_reset and not args.dry_run:
+        # Drop Data from AWS bucket
+        aws.delete_all(args.sync_table)
+        # drop backup data from volume path
+        shutil.rmtree(volume_path / str(args.sync_table))
+
     if args.sync_table == SyncTable.APP_DATA:
         asyncio.run(
             sync_app_data(
