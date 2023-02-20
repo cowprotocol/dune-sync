f088b1cece797df022fd661ff4b1b1934da05621
Ben Smith <bh2smith@gmail.com>
2023-02-20 16:58:46 +0100

    use normal join technic until we deem it insufficient

--- src/sql/orderbook/batch_rewards.sql
+++ src/sql/orderbook/batch_rewards.sql
@@ -1,39 +1,43 @@
-with hashed_observations as (SELECT block_number,
-                                    tx_hash,
-                                    effective_gas_price * gas_used as execution_cost,
-                                    surplus,
-                                    fee,
-                                    auction_id
-                             FROM settlement_observations so
-                                    LEFT OUTER JOIN LATERAL (
-                               SELECT tx_hash, solver, tx_nonce, tx_from
-                               FROM settlements s
-                               WHERE s.block_number = so.block_number
-                                 AND s.log_index = so.log_index
-                               LIMIT 1
-                               ) AS settlement ON TRUE
-                                    JOIN auction_transaction
-                                         ON settlement.tx_from = auction_transaction.tx_from
-                                           AND settlement.tx_nonce = auction_transaction.tx_nonce
-                             WHERE block_number > {{start_block}} AND block_number <= {{end_block}}),
+WITH hashed_observations AS (
+SELECT
+    -- settlement
+    tx_hash,
+    -- settlement_observations
+    block_number,
+    effective_gas_price * gas_used AS execution_cost,
+    surplus,
+    fee,
+    -- auction_transaction
+    auction_id
+FROM settlement_observations so
+JOIN settlements s
+  ON s.block_number = so.block_number
+  AND s.log_index = so.log_index
+JOIN auction_transaction at
+  ON s.tx_from = at.tx_from
+  AND s.tx_nonce = at.tx_nonce
+WHERE block_number > {{start_block}} AND block_number <= {{end_block}}
+),
 
-     reward_data as (select
-                       -- observations
-                       block_number,
-                       tx_hash,
-                       execution_cost,
-                       surplus,
-                       fee,
-                       surplus + fee - reference_score as reward_eth,
-                       -- scores
-                       winning_score,
-                       reference_score,
-                       -- participation
-                       participants
-                     from hashed_observations ho
-                            join settlement_scores ss
-                                 on ho.auction_id = ss.auction_id
-                            join auction_participants ap
-                                 on ho.auction_id = ap.auction_id)
+reward_data AS (
+  SELECT
+     -- observations
+     block_number,
+     tx_hash,
+     execution_cost,
+     surplus,
+     fee,
+     surplus + fee - reference_score AS reward_eth,
+     -- scores
+     winning_score,
+     reference_score,
+     -- participation
+     participants
+FROM hashed_observations ho
+JOIN settlement_scores ss
+  ON ho.auction_id = ss.auction_id
+JOIN auction_participants ap
+  ON ho.auction_id = ap.auction_id
+)
 
-select * from reward_data
+SELECT * FROM reward_data
