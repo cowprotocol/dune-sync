ed2b03dd9e07f7584fa21c402ae98dec82526234
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-05-17 15:25:32 +0200

    sync internal transfers (#37)

---
+++ src/models/token_imbalance_schema.py
@@ -0,0 +1,27 @@
+"""Model for Batch Rewards Data"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+from pandas import DataFrame
+
+
+@dataclass
+class TokenImbalance:
+    """
+    This class provides a transformation interface (to JSON) for Dataframe
+    """
+
+    @classmethod
+    def from_pdf_to_dune_records(cls, frame: DataFrame) -> list[dict[str, Any]]:
+        """Converts Pandas DataFrame into the expected stream type for Dune"""
+        return [
+            {
+                "block_number": int(row["block_number"]),
+                "tx_hash": row["tx_hash"],
+                "token": row["token"],
+                "amount": int(row["amount"]),
+            }
+            for row in frame.to_dict(orient="records")
+        ]
---
+++ src/sync/token_imbalance.py
@@ -0,0 +1,45 @@
+"""Main Entry point for token_imbalance sync"""
+from dune_client.file.interface import FileIO
+
+from src.fetch.postgres import PostgresFetcher
+from src.logger import set_log
+from src.models.block_range import BlockRange
+from src.models.tables import SyncTable
+from src.models.token_imbalance_schema import TokenImbalance
+from src.post.aws import AWSClient
+from src.sync.common import last_sync_block
+from src.sync.config import SyncConfig
+from src.sync.order_rewards import OrderbookDataHandler
+from src.sync.upload_handler import UploadHandler
+
+log = set_log(__name__)
+
+
+def sync_internal_imbalance(
+    aws: AWSClient, fetcher: PostgresFetcher, config: SyncConfig, dry_run: bool
+) -> None:
+    """Token Imbalance Sync Logic"""
+    sync_table = SyncTable.INTERNAL_IMBALANCE
+    block_range = BlockRange(
+        block_from=last_sync_block(
+            aws,
+            table=sync_table,
+            # TODO - determine and set the correct genesis block!
+            genesis_block=17236982,
+        ),
+        block_to=fetcher.get_latest_block(),
+    )
+    # TODO - Gap Detection (find missing txHashes and ensure they are accounted for!)
+    record_handler = OrderbookDataHandler(
+        file_manager=FileIO(config.volume_path / str(sync_table)),
+        block_range=block_range,
+        config=config,
+        data_list=TokenImbalance.from_pdf_to_dune_records(
+            fetcher.get_internal_imbalances(block_range)
+        ),
+        sync_table=sync_table,
+    )
+    UploadHandler(aws, record_handler, table=sync_table).write_and_upload_content(
+        dry_run
+    )
+    log.info(f"{sync_table} sync run completed successfully")
