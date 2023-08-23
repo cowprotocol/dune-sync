8a4905d2ae0c7ce4494304edc5814253d2125f1c
Martin Beckmann <martin.beckmann@gnosis.pm>
2023-08-23 09:18:22 +0200

    Check for appdata in the backend first

--- src/fetch/ipfs.py
+++ src/fetch/ipfs.py
@@ -124,21 +124,22 @@ class Cid:
                 cid = cls(app_hash)
 
                 first_seen_block = int(row["first_seen_block"])
-                # try fetching new format from IPFS
-                result = await cid.fetch_content(
-                    max_retries, previous_attempts, session, first_seen_block
+                # try fetching any format from backend (prod and staging)
+                result = cls.fetch_from_backend(
+                    app_hash, first_seen_block, previous_attempts
                 )
+
+
+                if isinstance(result, NotFoundContent):
+                    # try fetching new format from IPFS
+                    result = await cid.fetch_content(
+                        max_retries, previous_attempts, session, first_seen_block
+                    )
 
                 if isinstance(result, NotFoundContent):
                     # try fetching the old format from IPFS
                     result = await cls.old_schema(app_hash).fetch_content(
                         max_retries, previous_attempts, session, first_seen_block
-                    )
-
-                if isinstance(result, NotFoundContent):
-                    # try fetching any format from backend (prod and staging)
-                    result = cls.fetch_from_backend(
-                        app_hash, first_seen_block, previous_attempts
                     )
 
                 if isinstance(result, FoundContent):
