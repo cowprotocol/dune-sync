33425f7c978d9d98c7e71d813e5f9e57282db2ed
harisang <harisangelidakis@gmail.com>
2024-02-09 00:40:42 +0200

    format sql queries

--- src/sql/warehouse/token_imbalances.sql
+++ src/sql/warehouse/token_imbalances.sql
@@ -1,8 +1,11 @@
-select block_number,
-       concat('0x', encode(s.tx_hash, 'hex')) as tx_hash,
-       concat('0x', encode(token, 'hex'))     as token,
-       amount::text                           as amount
-from internalized_imbalances
-         join settlements s
-              on internalized_imbalances.tx_hash = s.tx_hash
-where block_number > {{start_block}} and block_number <= {{end_block}};
+select
+     block_number,
+     concat('0x', encode(s.tx_hash, 'hex')) as tx_hash,
+     concat('0x', encode(token, 'hex')) as token,
+     amount :: text as amount
+from
+     internalized_imbalances
+     join settlements s on internalized_imbalances.tx_hash = s.tx_hash
+where
+     block_number > {{start_block}}
+     and block_number <= {{end_block}};
