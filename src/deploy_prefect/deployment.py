e98dc57aa82dd0db99c1a9889208163223cbd6b0
= <bram@cow.fi>
2024-10-17 16:49:58 +0200

    Update flow

--- src/deploy_prefect/deployment.py
+++ src/deploy_prefect/deployment.py
@@ -148,8 +148,9 @@ def order_rewards() -> None:
 
 
 if __name__ == "__main__":
-    order_rewards.serve(
+    order_rewards.deploy(
         name="dune-sync-prod-order-rewards",
+        work_pool_name="cowbarn",
         cron="*/30 * * * *",  # Every 30 minutes
         tags=["solver", "dune-sync"],
         description="Run the dune sync order_rewards query",
