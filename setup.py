694eacd3b94b6b885f5ad2819b69554a3a530bda
= <bram@cow.fi>
2024-11-06 13:31:26 +0100

    Add setup.py so that the repo is installable with pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dune-sync',
+      version='0.1',
+      description='Components for syncing off-chain data with Dune Community Sources ',
+      url='https://github.com/cowprotocol/dune-sync',
+      author='cowprotocol',
+      author_email='info@cow.fi',
+      packages=['src'],
+      zip_safe=False)
