17628125514c943acffe5a74655ab1e1584a0893
= <bram@cow.fi>
2024-11-06 13:43:17 +0100

    Reformatting

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='dune-sync',
-      version='0.1',
-      description='Components for syncing off-chain data with Dune Community Sources ',
-      url='https://github.com/cowprotocol/dune-sync',
-      author='cowprotocol',
-      author_email='info@cow.fi',
-      packages=['src'],
-      zip_safe=False)
+setup(
+    name="dune-sync",
+    version="0.1",
+    description="Components for syncing off-chain data with Dune Community Sources ",
+    url="https://github.com/cowprotocol/dune-sync",
+    author="cowprotocol",
+    author_email="info@cow.fi",
+    packages=["src"],
+    zip_safe=False,
+)
