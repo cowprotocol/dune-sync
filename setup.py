111856503879c3b5ffaf42719f46c9b21e122011
= <bram@cow.fi>
2024-11-08 12:53:46 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -9,4 +9,15 @@ setup(
     packages=packages,
     package_dir={"dune_sync": "src"},
     include_package_data=True,
+    install_requires=[
+        "dune-client==1.7.4",
+        "psycopg2-binary>=2.9.3",
+        "python-dotenv>=0.20.0",
+        "requests>=2.28.1",
+        "pandas==2.1.4",
+        "ndjson>=0.3.1",
+        "py-multiformats-cid>=0.4.4",
+        "boto3>=1.26.12",
+        "SQLAlchemy<2.0",
+    ],
 )
