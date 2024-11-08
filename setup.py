1121c2fd3fb1312b1c1111ca1b569a1f78714b77
= <bram@cow.fi>
2024-11-08 09:39:26 +0100

    Formatting changes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-subpackages = find_packages('src')
-packages = ['dune_sync'] + ['dune_sync.' + p for p in subpackages]
+subpackages = find_packages("src")
+packages = ["dune_sync"] + ["dune_sync." + p for p in subpackages]
 
 setup(
-    name='dune_sync',
-    version='1.6.4',
+    name="dune_sync",
+    version="1.6.4",
     packages=packages,
-    package_dir={'dune_sync': 'src'},
+    package_dir={"dune_sync": "src"},
     include_package_data=True,
 )
