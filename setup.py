cfc5176f0d290ca877969d221c6514be4827492a
= <bram@cow.fi>
2024-11-11 17:26:10 +0100

    Automatically include requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from setuptools import setup, find_packages
 subpackages = find_packages("src")
 packages = ["src"] + ["src." + p for p in subpackages]
 
+
+def read_requirements(filename):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f.readlines() if line.strip()]
+
+
 setup(
     name="src",
     version="1.6.4",
@@ -18,15 +24,5 @@ setup(
             ["logging.conf"],
         )
     ],
-    install_requires=[
-        "dune-client==1.7.4",
-        "psycopg2-binary>=2.9.3",
-        "python-dotenv>=0.20.0",
-        "requests>=2.28.1",
-        "pandas==2.1.4",
-        "ndjson>=0.3.1",
-        "py-multiformats-cid>=0.4.4",
-        "boto3>=1.26.12",
-        "SQLAlchemy<2.0",
-    ],
+    install_requires=read_requirements("requirements/prod.txt"),
 )
