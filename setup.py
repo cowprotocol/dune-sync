982afcc104a88d6cf3dabcd47b5c586ade7aea10
= <bram@cow.fi>
2024-11-11 16:45:16 +0100

    Formatting

--- setup.py
+++ setup.py
@@ -10,10 +10,14 @@ setup(
     packages=packages,
     package_dir={"dune_sync": "src/dune_sync"},
     include_package_data=True,
-    data_files=[(
-        os.path.join('lib', 'python{0}.{1}'.format(*os.sys.version_info[:2]), 'site-packages'),
-        ['logging.conf']
-    )],
+    data_files=[
+        (
+            os.path.join(
+                "lib", "python{0}.{1}".format(*os.sys.version_info[:2]), "site-packages"
+            ),
+            ["logging.conf"],
+        )
+    ],
     install_requires=[
         "dune-client==1.7.4",
         "psycopg2-binary>=2.9.3",
