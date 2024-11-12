82fab7984bcf98e6d129dfd82b79a16f357034de
= <bram@cow.fi>
2024-11-12 14:52:27 +0100

    Update setup.py to include sql files

--- setup.py
+++ setup.py
@@ -4,10 +4,18 @@ from setuptools import setup, find_packages
 subpackages = find_packages("src")
 packages = ["src"] + ["src." + p for p in subpackages]
 
-
 def read_requirements(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f.readlines() if line.strip()]
+
+def get_sql_files(directory):
+    sql_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".sql"):
+                # Add 'src/' prefix to the path
+                sql_files.append(os.path.join('src', os.path.relpath(os.path.join(root, file), start="src")))
+    return sql_files
 
 
 setup(
@@ -22,7 +30,13 @@ setup(
                 "lib", "python{0}.{1}".format(*os.sys.version_info[:2]), "site-packages"
             ),
             ["logging.conf"],
-        )
+        ),
+        (
+            os.path.join(
+                "lib", "python{0}.{1}".format(*os.sys.version_info[:2]), "site-packages", "src", "sql"
+            ),
+            get_sql_files("src/sql"),
+        ),
     ],
     install_requires=read_requirements("requirements/prod.txt"),
 )
